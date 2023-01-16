b0d32ed554a08dd0333971ee875ccb30e1001db9
Masein <imasein@gmail.com>
2023-01-16 14:42:33 +0330

    initial commit

---
+++ CoreML-in-ARKit/ViewController.swift
@@ -0,0 +1,286 @@
+//
+//  ViewController.swift
+//  CoreML-in-ARKit
+//
+//  Created by Yehor Chernenko on 01.08.2020.
+//  Copyright © 2020 Yehor Chernenko. All rights reserved.
+//
+
+import UIKit
+import Vision
+import ARKit
+
+
+class ViewController: UIViewController {
+    var objectDetectionService = ObjectDetectionService()
+    let throttler = Throttler(minimumDelay: 1, queue: .global(qos: .userInteractive))
+    var isLoopShouldContinue = true
+    var lastLocation: SCNVector3?
+    
+    let imagePredictor = ImagePredictor()
+    /// The largest number of predictions the main view controller displays the user.
+    let predictionsToShow = 2
+
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var sessionInfoLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        sceneView.session.delegate = self
+        sceneView.scene = SCNScene()
+        
+        // Enable Default Lighting - makes the 3D text a bit poppier.
+        sceneView.autoenablesDefaultLighting = true
+        
+        // Debug
+        sceneView.showsStatistics = true
+        sceneView.debugOptions = [.showFeaturePoints]
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        startSession()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        stopSession()
+    }
+    
+    private func startSession(resetTracking: Bool = false) {
+        guard ARWorldTrackingConfiguration.isSupported else {
+            assertionFailure("ARKit is not supported")
+            return
+        }
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        
+        if resetTracking {
+            sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        } else {
+            sceneView.session.run(configuration)
+        }
+    }
+    
+    func stopSession() {
+        sceneView.session.pause()
+    }
+    
+    func loopObjectDetection() {
+        throttler.throttle { [weak self] in
+            guard let self = self else { return }
+            
+            if self.isLoopShouldContinue {
+                self.performDetection()
+            }
+            self.loopObjectDetection()
+        }
+    }
+    
+    func performDetection() {
+        // for image using MugAndBottle
+//        let image = imageFrom(scene: sceneView)
+//        classifyImage(image)
+        
+        guard let pixelBuffer = sceneView.session.currentFrame?.capturedImage else { return }
+
+        objectDetectionService.detect(on: .init(pixelBuffer: pixelBuffer)) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let response):
+                print("response: \(response)")
+                let rectOfInterest = VNImageRectForNormalizedRect(
+                    response.boundingBox,
+                    Int(self.sceneView.bounds.width),
+                    Int(self.sceneView.bounds.height))
+                self.addAnnotation(rectOfInterest: rectOfInterest,
+                                   text: response.classification)
+
+            case .failure(let error):
+                print("this is detection error: \(error)")
+                break
+            }
+        }
+    }
+    
+    func addAnnotation(rectOfInterest rect: CGRect, text: String) {
+        let point = CGPoint(x: rect.midX, y: rect.midY)
+        
+        let scnHitTestResults = sceneView.hitTest(point,
+                                                  options: [SCNHitTestOption.searchMode: SCNHitTestSearchMode.all.rawValue])
+        guard !scnHitTestResults.contains(where: { $0.node.name == BubbleNode.name }) else { return }
+        
+        guard let raycastQuery = sceneView.raycastQuery(from: point,
+                                                        allowing: .existingPlaneInfinite,
+                                                        alignment: .horizontal),
+              let raycastResult = sceneView.session.raycast(raycastQuery).first else { return }
+        let position = SCNVector3(raycastResult.worldTransform.columns.3.x,
+                                  raycastResult.worldTransform.columns.3.y,
+                                  raycastResult.worldTransform.columns.3.z)
+
+        guard let cameraPosition = sceneView.pointOfView?.position else { return }
+        let distance = (position - cameraPosition).length()
+        guard distance <= 0.5 else { return }
+        
+        let bubbleNode = BubbleNode(text: text)
+        bubbleNode.worldPosition = position
+        
+        sceneView.prepare([bubbleNode]) { [weak self] success in
+            if success {
+                self?.sceneView.scene.rootNode.addChildNode(bubbleNode)
+            }
+        }
+    }
+
+    private func onSessionUpdate(for frame: ARFrame, trackingState: ARCamera.TrackingState) {
+        isLoopShouldContinue = false
+
+        // Update the UI to provide feedback on the state of the AR experience.
+        let message: String
+        
+        switch trackingState {
+        case .normal where frame.anchors.isEmpty:
+            // No planes detected; provide instructions for this app's AR interactions.
+            message = "Move the device around to detect horizontal and vertical surfaces."
+            
+        case .notAvailable:
+            message = "Tracking unavailable."
+            
+        case .limited(.excessiveMotion):
+            message = "Tracking limited - Move the device more slowly."
+            
+        case .limited(.insufficientFeatures):
+            message = "Tracking limited - Point the device at an area with visible surface detail, or improve lighting conditions."
+            
+        case .limited(.initializing):
+            message = "Initializing AR session."
+            
+        default:
+            // No feedback needed when tracking is normal and planes are visible.
+            // (Nor when in unreachable limited-tracking states.)
+            message = ""
+            isLoopShouldContinue = true
+            loopObjectDetection()
+        }
+        
+        sessionInfoLabel.text = message
+        sessionInfoLabel.isHidden = message.isEmpty
+    }
+}
+
+extension ViewController: ARSessionDelegate {
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        guard let frame = session.currentFrame else { return }
+        onSessionUpdate(for: frame, trackingState: camera.trackingState)
+    }
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+        onSessionUpdate(for: frame, trackingState: frame.camera.trackingState)
+    }
+    
+    func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+        onSessionUpdate(for: frame, trackingState: frame.camera.trackingState)
+    }
+    
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        let transform = SCNMatrix4(frame.camera.transform)
+        let orientation = SCNVector3(-transform.m31, -transform.m32, transform.m33)
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let currentPositionOfCamera = orientation + location
+        
+        if let lastLocation = lastLocation {
+            let speed = (lastLocation - currentPositionOfCamera).length()
+            isLoopShouldContinue = speed < 0.0025
+        }
+        lastLocation = currentPositionOfCamera
+    }
+    
+    // MARK: - ARSessionObserver
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay.
+        sessionInfoLabel.text = "Session was interrupted"
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required.
+        sessionInfoLabel.text = "Session interruption ended"
+        startSession(resetTracking: true)
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        sessionInfoLabel.text = "Session error: \(error.localizedDescription)"
+    }
+}
+
+extension ViewController: ARSCNViewDelegate { }
+
+extension ViewController {
+    // MARK: Image prediction methods
+    /// Sends a photo to the Image Predictor to get a prediction of its content.
+    /// - Parameter image: A photo.
+    private func classifyImage(_ image: UIImage) {
+        do {
+            try self.imagePredictor.makePredictions(for: image,
+                                                    completionHandler: imagePredictionHandler)
+        } catch {
+            print("Vision was unable to make a prediction...\n\n\(error.localizedDescription)")
+        }
+    }
+
+    /// The method the Image Predictor calls when its image classifier model generates a prediction.
+    /// - Parameter predictions: An array of predictions.
+    /// - Tag: imagePredictionHandler
+    private func imagePredictionHandler(_ predictions: [ImagePredictor.Prediction]?) {
+        guard let predictions = predictions else {
+            print("No predictions. (Check console log.)")
+            return
+        }
+
+        let formattedPredictions = formatPredictions(predictions)
+
+        let predictionString = formattedPredictions.joined(separator: "\n")
+        print("stringgg \(predictionString)")
+//        let rectOfInterest = VNImageRectForNormalizedRect(
+//                            forma,
+//                            Int(self.sceneView.bounds.width),
+//                            Int(self.sceneView.bounds.height))
+//                        self.addAnnotation(rectOfInterest: rectOfInterest,
+//                                           text: predictionString)
+    }
+
+    /// Converts a prediction's observations into human-readable strings.
+    /// - Parameter observations: The classification observations from a Vision request.
+    /// - Tag: formatPredictions
+    private func formatPredictions(_ predictions: [ImagePredictor.Prediction]) -> [String] {
+        // Vision sorts the classifications in descending confidence order.
+        let topPredictions: [String] = predictions.prefix(predictionsToShow).map { prediction in
+            var name = prediction.classification
+
+            // For classifications with more than one name, keep the one before the first comma.
+            if let firstComma = name.firstIndex(of: ",") {
+                name = String(name.prefix(upTo: firstComma))
+            }
+
+            return "\(name) - \(prediction.confidencePercentage)%"
+        }
+
+        return topPredictions
+    }
+    
+    func imageFrom(scene:ARSCNView) -> UIImage {
+        DispatchQueue.main.async {
+            UIGraphicsBeginImageContextWithOptions(scene.bounds.size, scene.isOpaque, 0.0)
+            scene.drawHierarchy(in: scene.bounds, afterScreenUpdates: false)
+        }
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return UIImage(cgImage: (image?.cgImage)!)
+    }
+}
